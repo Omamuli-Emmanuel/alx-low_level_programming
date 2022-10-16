283c3affec1d74cddd28c6dc2c0b69bb0eef9351
Emmanuel Omamuli <omamuli.emmanuel@gmail.com>
2022-10-16 12:20:17 -0700

    updated sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Enrty point
- * Description : 'prints sizes using printf'
+ * Description: 'prints sizes using printf'
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -12,10 +13,10 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of double: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of char: %zu bytes(s)\n", sizeof(charType));
 
 	return (0);
 }
