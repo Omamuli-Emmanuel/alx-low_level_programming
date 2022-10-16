21221a5fa98f59988787e33e5e2103923811725d
Emmanuel Omamuli <omamuli.emmanuel@gmail.com>
2022-10-16 11:49:30 -0700

    Print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'prints a dtring using fputs'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,", stdout);
+	return (0);
+}
