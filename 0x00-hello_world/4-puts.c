e9fa67bb1683f5a47be80a07b18cacc8869c7410
Emmanuel Omamuli <omamuli.emmanuel@gmail.com>
2022-10-16 11:41:04 -0700

    Trailling white space removed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'prints a dtring using fputs'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	fputs("\"Programming is like building a multilingual puzzle \n", stdout);
+	fputs("\"Programming is like building a multilingual puzzle\n", stdout);
 
 	return (0);
 }
