e2fefc5ef60d2abae3e68317d8255f9257b6eae9
Emmanuel Omamuli <omamuli.emmanuel@gmail.com>
2022-10-17 21:20:34 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(0)
+{
+	char leet;
+
+	for (leet = 'a'; leet <= 'z'; leet++)
+		putchar(leet);
+	for (leet = 'A'; leet <= 'Z'; leet++)
+		putchar(leet);
+	putchar('\n');
+
+	return (0);
+}
