3d5ce977382c0f2fd3a859333848e040adc9d8a8
Emmanuel Omamuli <omamuli.emmanuel@gmail.com>
2022-10-19 22:00:28 -0700

    puthar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - returns c to stdout
+ * @c: character to print
+ * Return: On success 1.
+ * On error, -1 is printed
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
